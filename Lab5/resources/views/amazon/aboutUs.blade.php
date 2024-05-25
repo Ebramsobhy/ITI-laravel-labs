ce6c5add0329bc3632d2bf290dbf33fbbac27f89
Ebram Sobhy <116510911+Ebramsobhy@users.noreply.github.com>
2024-05-25 22:45:28 +0300

    Delete Lab5 directory

--- Lab5/resources/views/amazon/aboutUs.blade.php
+++
@@ -1,3 +0,0 @@
-@extends('layouts.app')
-@section('title') AboutUS @endsection
-@section('body') <h1>AboutUS</h1> @endsection
--- Lab5/resources/views/amazon/contactUs.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('title') ContactUS @endsection
-
-@section('body') <h1>ContactUS</h1> @endsection
